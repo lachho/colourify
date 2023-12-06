f59cc5aa9013f63aa4d63d1ee629410b87f797ee
lachho <143239409+lachho@users.noreply.github.com>
2023-12-06 15:45:21 +1100

    Update index.js

--- index.js
+++ index.js
@@ -13,14 +13,32 @@ function newColour() {
   return '#' + Math.floor(Math.random()*16777215).toString(16);
 }
 
-function shape() {
-    const index = Math.floor(Math.random() * 3);
-    const shape = document.getElementById('shapeID');
-    if (index == 0) {
-        shape.borderRadius = "0%";
-    } else if (index == 1) {
-        shape.borderRadius = "25%"
-    } else {
-        shape.borderRadius = "50%";
+function loop(shape, borderRadius, change, i) {
+    const newBorderRadius = parseInt(borderRadius) + change * i;
+    shape.style.borderRadius = `${newBorderRadius}%`;
+    i++;
+    if (i % 15 == 0) {
+        random();
     }
+    if (i <= 50) {
+        setTimeout(() => loop(shape, borderRadius, change, i), 10);
+    }
+}
+
+function shape() {
+    const shape = document.getElementById('shapeID');
+    const style = window.getComputedStyle(shape);
+    let borderRadius = style.getPropertyValue('border-radius');
+    
+    let change;
+    if (parseInt(borderRadius) == 0) {
+        change = 1;
+    }
+    else {
+        change = -1;
+    }
+
+    let i = 0;
+    loop(shape, borderRadius, change, i);
+
 }
