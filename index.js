21ada71a5b1e42ec6c5290744ae91784e9e9aa6f
lachho <143239409+lachho@users.noreply.github.com>
2023-12-06 15:02:15 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 function paint(colour) {
-  const circle = document.getElementById('circleID');
-  circle.style = `background-color:${colour}`;
+  const shape = document.getElementById('shapeID');
+  shape.style = `background-color:${colour}`;
 }
 
 function random() {
-	const circle = document.getElementById('circleID');
-	circle.style = `background-color:${newColour()}`
+	const shape = document.getElementById('shapeID');
+	shape.style = `background-color:${newColour()}`
 	document.body.style.backgroundColor = newColour();
 }
 
 function newColour() {
   return '#' + Math.floor(Math.random()*16777215).toString(16);
+}
+
+function shape() {
+    const index = Math.floor(Math.random() * 3);
+    const shape = document.getElementById('shapeID');
+    if (index == 0) {
+        shape.borderRadius = "0%";
+    } else if (index == 1) {
+        shape.borderRadius = "25%"
+    } else {
+        shape.borderRadius = "50%";
+    }
 }
