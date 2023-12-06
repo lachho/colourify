c1eba207379cf72087c1003f6d11f3df2eb80b6e
lachho <143239409+lachho@users.noreply.github.com>
2023-12-07 10:54:11 +1100

    updated comments for clarity

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
+// This function finds the shape and changes the background colour to the input.
 function paint(colour) {
   const shape = document.getElementById('shapeID');
   shape.style = `background-color:${colour}`;
 }
 
+// This function finds the shape and changes the background colour to a random colour.
 function random() {
 	const shape = document.getElementById('shapeID');
 	shape.style = `background-color:${newColour()}`
 	document.body.style.backgroundColor = newColour();
 }
 
+// This function randomly creates a new colour.
 function newColour() {
   return '#' + Math.floor(Math.random()*16777215).toString(16);
 }
 
+// This function recursively changes the border radius to round/straigten it.
 function loop(shape, borderRadius, change, i) {
     const newBorderRadius = parseInt(borderRadius) + change * i;
     shape.style.borderRadius = `${newBorderRadius}%`;
@@ -25,6 +29,7 @@ function loop(shape, borderRadius, change, i) {
     }
 }
 
+// This function visually changes the shape from circle to square and back.
 function shape() {
     const shape = document.getElementById('shapeID');
     const style = window.getComputedStyle(shape);
